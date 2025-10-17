8e8a33bd9aad3b0033d2f312cb420c260992257a
Greeeeeezly <zolotarev.grisha.2012@gmail.com>
2025-10-17 19:53:50 +0300

    add MessageScreen, deleted comments

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -1,10 +1,37 @@
 package com.example;
 
+import com.example.client.gui.MessageScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class ExampleModClient implements ClientModInitializer {
+	private static final String KEY_CATEGORY = "category.modid.controls";
+	private static final String KEY_OPEN_SCREEN = "key.modid.open_message_screen";
+
+	private KeyBinding openScreenKey;
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		openScreenKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			KEY_OPEN_SCREEN,
+			InputUtil.Type.KEYSYM,
+			GLFW.GLFW_KEY_M,
+			KEY_CATEGORY
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(this::handleKeyBinding);
+	}
+
+	private void handleKeyBinding(MinecraftClient client) {
+		while (openScreenKey.wasPressed()) {
+			if (client.currentScreen == null) {
+				client.setScreen(new MessageScreen());
+			}
+		}
 	}
 }
